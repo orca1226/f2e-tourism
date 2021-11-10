f5113260b0b191cf2427921091ea68b6679681d2
Lillian Lin <lillian2179@gmail.com>
2021-11-10 19:55:15 +0800

    add tailwin.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+
+    },
+    fontFamily: {
+      sans: ['Noto-Sans-TC', 'sans-serif']
+    },
+    extend: {
+
+    }
+  },
+  variants: {
+    extend: {
+    }
+  },
+  plugins: []
+}
