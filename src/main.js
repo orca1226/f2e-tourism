963515ef5f667c7521d5508fd8bbd7f29924e6be
Lillian Lin <lillian2179@yahoo.com.tw>
2021-11-14 11:07:27 +0800

    npm - install vue-horizontal

--- src/main.js
+++ src/main.js
@@ -4,7 +4,11 @@ import router from './router'
 import api from './api/index.js'
 import request from './api/request.js'
 
+// 套件
+import VueHorizontal from 'vue-horizontal'
+
 Vue.config.productionTip = false
+Vue.use(VueHorizontal)
 
 // 在建構實例後才加prototype會無法掛載...
 Vue.prototype.$api = api
